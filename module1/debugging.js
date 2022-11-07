f75fbcb2ed70d14db37531c6ac4742add2ad142e
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-07 14:08:41 +0100

    commented

--- module1/debugging.js
+++ module1/debugging.js
@@ -1,14 +1,22 @@
+
 /**
- * Test function
+ * Counts to 1000
  */
-function complexCalculation() {
+export function complexCalculation() {
     for (let i = 0; i < 1000; i++) {
       // console.log(i);
     }
   };
-  /**
-   *Test how much time it takes to run a function 
-   */
+
+
+  //Test how much time it takes to run a function 
   console.time('myTimer');
-  complexCalculation();
+  //insert function:
   console.timeEnd('myTimer');
+
+  /**
+ * Adds two numbers together
+ */
+export function addNumbers(a, b) {
+  return a + b;
+}
