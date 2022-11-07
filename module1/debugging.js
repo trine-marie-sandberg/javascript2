8bbc897cc0b2cae2510eb85b929b361a1d72bd82
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-07 13:53:04 +0100

    added JSDocs comment

--- module1/debugging.js
+++ module1/debugging.js
@@ -1,10 +1,14 @@
-//Testfunction
+/**
+ * Test function
+ */
 function complexCalculation() {
     for (let i = 0; i < 1000; i++) {
       // console.log(i);
     }
   };
-  //Test how much time it takes to run a function
+  /**
+   *Test how much time it takes to run a function 
+   */
   console.time('myTimer');
   complexCalculation();
   console.timeEnd('myTimer');
