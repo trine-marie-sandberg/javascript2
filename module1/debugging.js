bb586ceaa90f85fd01b8fa249c66b53318223e0a
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-07 13:01:52 +0100

    initial commit

---
+++ module1/debugging.js
@@ -0,0 +1,10 @@
+//Testfunction
+function complexCalculation() {
+    for (let i = 0; i < 1000; i++) {
+      // console.log(i);
+    }
+  };
+  //Test how much time it takes to run a function
+  console.time('myTimer');
+  complexCalculation();
+  console.timeEnd('myTimer');
