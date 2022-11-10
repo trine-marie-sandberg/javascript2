b698e4e3f06b154c10936c61ee803549d96a0e0d
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-10 16:04:04 +0100

    added script for documentation and tested it

--- debugging-jsdocs.js
+++ debugging-jsdocs.js
@@ -16,15 +16,19 @@
   console.timeEnd('myTimer');
 
   /**
+ * Number or string containing number
+ * @typedef {(number|string)} NumberLike
  * Adds two numbers together
  * @param {number} a First value
  * @param {number} b Second value
+ * @param {NumberLike} a
+ * @param {NumberLike} b
  * @returns {number} Sum of params
+ * Example function
  * @param {dataType} [optionalParameterName=10] optionalParameterDescription
-  * Number or string containing number
- * @typedef {(number|string)} NumberLike
- *  * ```js
- * // Add two numbers together
+ * @example
+ * ```js
+ * // Add two numbers together yay
  * const a = 1;
  * const b = 2;
  * const sum = addNumbers(a, b);
