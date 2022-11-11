0910459e7fbdb1ad8650251a46e4fa32ac8e4077
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-11 15:02:57 +0100

    lesson notes

--- module1/map.js
+++ module1/map.js
@@ -1,5 +1,8 @@
-//Map is an object and holds key/value pairs. 
-//A normal objects order is unreliable, but the order in a Map object is remembered.
+//Map is an object and holds key/value pairs.
+//However there are some differences: 
+//A normal objects order is unreliable, but the order in a Map object remains the way wich they are added.
+//A map's key can be any value. Included function, objects and primitives (object keys must be string or symbol).
+
 const myMap = new Map();
 
 //Set key/value
@@ -16,3 +19,13 @@ newMap.set("key1", 1);
 newMap.set("key2", 2);
 //Log number of items in a Map
 console.log(newMap.size);
+
+//One cannot directly use JSON.stringify() on a map.
+//https://stackoverflow.com/questions/29085197/how-do-you-json-stringify-an-es6-map 
+
+//LESSON TASK
+const users = new Map();
+users.set(12, {firstName: "Ola", lastName: "Nordmann"});
+console.log(users.size);
+
+users.set(50, {firstName: "Kari",})
