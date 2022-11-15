f8f3410c258eef52a988846fa6e773204500ea9a
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-15 14:18:51 +0100

    lesson notes

--- module2/array-methods.js
+++ module2/array-methods.js
@@ -3,5 +3,23 @@
 const values = [100, 200, 300, 400, 500];
 
 const newArray = values.map(function (element, index, array) {
-  // Modify and return element here
+  console.log(element, index, array)
 });
+
+//Practical example
+const ourDogs = [
+  {
+    owner: "Tobias, Trine",
+    dogs: "Anton, Simba"
+  },
+  {
+    owner: "Mona",
+    dogs: "Emmy, Kelvin"
+  },
+];
+
+const familyDogs = ourDogs.map((dogsAndOwner) => {
+  return "Owner: " + dogsAndOwner.owner + " " + "Dogs: " + dogsAndOwner.dogs;
+});
+
+console.log(familyDogs)
