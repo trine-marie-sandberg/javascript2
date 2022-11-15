a022ef16559b9d2af8820e89390addcb466ed6dd
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-15 14:50:18 +0100

    lesson notes

---
+++ module2/filter.js
@@ -0,0 +1,17 @@
+//The filter method loops through the array and filters out items wich are then returned as a new array
+//EXAMPLE
+const values = [100, 200, 300, 400, 500];
+
+const newArray = values.filter((element, index, array) => {
+  // Return true if you want to keep the item in the array being returned
+  // Return false if you don't want the item to be included in the array being returned
+});
+
+//PARAMETERS
+//element <- the element we are working with
+//index
+//array <- The original array
+
+//Implisit false return
+//The filter method has an implicit false return meaning that it will always return false if you haven’t returned a value
+//In most cases, you usually only have to check for true conditions
--- module2/map.js
+++ module2/map.js
@@ -1,10 +1,22 @@
+//ARRAY METHODS
+//Each time our Array Methods loop, it runs a callback function
+
 //MAP METHOD
 
 const values = [100, 200, 300, 400, 500];
-
+//Most of the time using array methods, we use anonymous or arrow function
 const newArray = values.map(function (element, index, array) {
-  console.log(element, index, array)
+  console.log(element, index, array);
 });
+
+
+const values2 = [100, 200, 300, 400, 500];
+//Separate callback function uses a named function:
+function callbackFn(element, index, array) {
+  // Modify and return element here
+};
+const newArray2 = values.map(callbackFn);
+
 
 //Practical example
 const ourDogs = [
