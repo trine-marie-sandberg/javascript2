f46326405a9dbe02f2d8c786b9474ce3db702d4a
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-17 15:22:46 +0100

    lesson notes

--- module2/reduce.js
+++ module2/reduce.js
@@ -1,1 +1,19 @@
 //Unlike most array methods, reduce returns a single variable
+//You can then modify this single variable each loop which must also be returned each loop
+//This single variable then becomes the final result returned by the reduce array method once it has finished looping over all of the elements
+//This single variable can be considered an “accumulator” or “total” as you typically add to it on each loop
+// Numbers that we will loop through
+const values = [2, 3, 5, 10, 20];
+
+const sum = values.reduce((total, value) => {
+  // Add our current element from the array to our 'total' accumulator
+  total += value;
+  // We have to return our 'total' accumulator
+  return total;
+
+  // You can see we are setting an initial value of 0 after the callback function
+}, 0);
+
+console.log(sum);
+// Returns:
+// 40
