7dcfb4cef681505c507621f072d08144065af0a3
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-20 15:12:40 +0100

    lesson notes

---
+++ module4/async.js
@@ -0,0 +1,4 @@
+//Asynchronous code is code that can run independently of our program flow
+//Unlike Synchronous code wich runs in sequence (line after line) 
+//Asynchronous code will run without blocking or blocking the main program
+console.log("test")
