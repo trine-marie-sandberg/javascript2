c50198e2e526cc871bae1dffe43ffdc3baeeae6c
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-21 15:59:39 +0100

    lesson notes

---
+++ module4/await.js
@@ -0,0 +1,4 @@
+//Async/Await allows us to writhe our code in a sequential way while still handling asynchronous code
+//async: This keyword must be added to the start of a function declaration so that it becomes an “async” function
+//We have to include this to make use of the await keyword
+//await: This keyword must be added at the start of any function call that needs to be dealt with asynchronously
