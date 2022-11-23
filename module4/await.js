11ef91da0fceb5b6540a439fe2d998ad2044ef4d
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-23 13:05:12 +0100

    lesson notes

--- module4/await.js
+++ module4/await.js
@@ -2,3 +2,25 @@
 //async: This keyword must be added to the start of a function declaration so that it becomes an “async” function
 //We have to include this to make use of the await keyword
 //await: This keyword must be added at the start of any function call that needs to be dealt with asynchronously
+
+// Fetches data from the URL passed in
+async function doFetch(url) {
+    const data = await fetch(url);
+    const json = await data.json();
+    return json;
+  }
+  
+  // Our main function that will call the data
+  // using await. If we don't use await then we will
+  // instead get a promise
+  async function main() {
+    const articles = await doFetch(
+      'https://jsonplaceholder.typicode.com/todos/1',
+    );
+    console.log(articles);
+    // Returns:
+    // {userId: 1, id: 1, title: 'delectus aut autem', completed: false}
+  }
+  
+  // Execute our code
+  main();
