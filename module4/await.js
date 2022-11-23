4468bde60d0c3521b51cce1d0d444bba89e9b2e9
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-23 13:26:10 +0100

    lesson notes

--- module4/await.js
+++ module4/await.js
@@ -24,3 +24,48 @@ async function doFetch(url) {
   
   // Execute our code
   main();
+
+//try: This is our main code that we attempt to run, such as fetching data from an API
+//catch: This block of code will only execute if there was a problem with the code in the try block, such as an error being thrown
+//finally: This is an optional block of code which will run regardless of whether the try block was successful
+  
+//Multiple parallel requests
+//We can use the Promise.all() helper function to get data from multiple API end points
+//It takes multiple promises and then returns a single array containing all of the results as different items in the array
+//If we were to use async await, then we would have to wait for each request to finish sequentially, which may not be efficient
+
+const promise1 = Promise.resolve('Promise 1 result');
+const promise2 = Promise.resolve('Promise 2 result');
+const promise3 = Promise.resolve('Promise 3 result');
+
+Promise.all([promise1, promise2, promise3]).then((values) => {
+  console.log(values);
+});
+
+//PRACTICAL EXAMPLE
+async function doFetch(url) {
+    try {
+      const data = await fetch(url);
+      const json = await data.json();
+      return json;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  let dataArray = [];
+
+  async function main() {
+    
+    const data = await Promise.all([
+      doFetch('https://jsonplaceholder.typicode.com/todos/1'),
+      doFetch('https://jsonplaceholder.typicode.com/todos/2'),
+      doFetch('https://jsonplaceholder.typicode.com/todos/3'),
+    ]);
+
+    dataArray.push(data);
+    return dataArray; 
+  };
+  
+  main();
+  console.log(dataArray);
