326e02917d5a2b26954b2a7e237bb52a9650c4bf
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-24 15:20:43 +0100

    lesson notes

--- module4/post.js
+++ module4/post.js
@@ -1,1 +1,14 @@
 //The GET request mode is the default mode when doing a fetch, but for POST we need to include a method
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
