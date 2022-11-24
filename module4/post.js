b8c91b691e397faa1a0a26e47596da37d4e44715
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-24 15:29:40 +0100

    lesson notes

--- module4/post.js
+++ module4/post.js
@@ -12,3 +12,20 @@ fetch('https://jsonplaceholder.typicode.com/posts', {
 })
   .then((response) => response.json())
   .then((json) => console.log(json));
+
+  //The request options object are saved in a variable 
+  const requestOptionsObject = {
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  };
+                        //And passed into the fetch method here👇
+  fetch('https://jsonplaceholder.typicode.com/posts', requestOptionsObject)
+    .then((response) => response.json())
+    .then((json) => console.log(json));
