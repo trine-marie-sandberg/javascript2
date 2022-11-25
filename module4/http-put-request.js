3c6e8aefb55bee439fee3838d4ab1beeffcc93db
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-25 21:21:35 +0100

    lesson notes

---
+++ module4/http-put-request.js
@@ -0,0 +1,14 @@
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: 'PUT',
+  body: JSON.stringify({
+    id: 1,
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
