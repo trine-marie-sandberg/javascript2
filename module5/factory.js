744dd10eb40a4f2db46db545194065975b10a995
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-27 20:43:26 +0100

    lesson task

--- module5/factory.js
+++ module5/factory.js
@@ -19,9 +19,9 @@ function createPerson(firstName, lastName, country, city) {
   //LESSON TASK
   function createAnimal(name, species, food) {
     return {
-        name: name,
-        species: species,
-        food: food,
+        // name: name,
+        // species: species,
+        // food: food,
         animalEats: function () {
             console.log(`${name} is a ${species} and eats ${food} food`);
         },
