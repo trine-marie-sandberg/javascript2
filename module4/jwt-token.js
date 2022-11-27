8542813e6015c34e79e2beae907608e17771432e
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-27 14:38:13 +0100

    lesson notes

--- module4/jwt-token.js
+++ module4/jwt-token.js
@@ -1,22 +1,24 @@
-//1. Register an account
+//1. REGISTER USER
 const API_BASE_URL = "https://nf-api.onrender.com";
-const apiRegisterUrl = "/api/v1/social/auth/register";
 
 async function registerUser(url, userData) {
+
     console.log(url, userData)
+
     try {
 
         //2. do api call
         const postData = {
-            method: "POST",
+            method: 'POST',
             headers: {
-                "Content-Type": "application/json",
+                'Content-Type': 'application/json',
             },
             body: JSON.stringify(userData),
         };
 
         const response = await fetch(url, postData);
         const json = await response.json();
+        console.log(json)
 
     } catch (error) {
 
@@ -25,14 +27,42 @@ async function registerUser(url, userData) {
 };
 
 const userToRegister = {
-    name: "Anton_Hollerudhagen",
-    email: "anton.email@hollerudhagen.com",
-    password: "blackLab10"
+    name: 'anton_hollerudhagen',
+    email: 'anton@noroff.no',
+    password: 'blackLab10',
 };
 
-const registerUrl = `${API_BASE_URL}${apiRegisterUrl}`;
-console.log(registerUrl)
+const registerUrl = `${API_BASE_URL}/api/v1/social/auth/register`;
 
-registerUser(registerUrl, userToRegister);
+//registerUser(registerUrl, userToRegister);
 
-///
+//LOGIN USER
+const loginEndpoint = "/api/v1/social/auth/login";
+const loginUrl = API_BASE_URL + loginEndpoint;
+
+const userToLogin = {
+    email: 'anton@noroff.no',
+    password: 'blackLab10',
+};
+
+async function loginUser(url, userData) {
+    //
+    try {
+        console.log(url, userData)
+        const postData = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData),
+        };
+        const response = await fetch(url, postData);
+        console.log(response)
+        const json = await response.json();
+        console.log(json)
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+loginUser(loginUrl, userToLogin);
