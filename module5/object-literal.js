59e4a8f8159830187f617815926246b7ecf9967b
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-27 20:55:32 +0100

    lesson notes

--- module5/object-literal.js
+++ module5/object-literal.js
@@ -19,5 +19,29 @@ const shoppingCart = {
   
   // Add an item to the cart
   shoppingCart.addToCart({ name: 'Milk', price: 9.99 });
+  shoppingCart.addToCart({ name: 'Egg x 12', price: 12.0 });
   // Display the cart contents
   shoppingCart.getCartItems();
+
+  //SETTERS AND GETTERS
+  //ECMAScript 5 (ES5 2009) introduced getters and setters to JavaScript
+  //A getter gets a value back for us whereas a setter sets a value for us
+  //example
+  const user = {
+    _firstName: 'John',
+  
+    // The 'setter'
+    set firstName(newFirstName) {
+      this._firstName = newFirstName;
+    },
+  
+    // The 'getter'
+    get firstName() {
+      return this._firstName;
+    },
+  };
+  
+  // Use the 'firstName' setter
+  user.firstName = 'Ola';
+  // Use the 'firstName' getter
+  console.log(user.firstName);
