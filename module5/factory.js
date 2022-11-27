21949ae5397d38ddd4a1d6b105d4986fb5760c5c
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-27 20:40:05 +0100

    lesson task

---
+++ module5/factory.js
@@ -0,0 +1,32 @@
+//FACTORY FUNCTION EXAMPLE
+function createPerson(firstName, lastName, country, city) {
+    return {
+      firstName: firstName,
+      lastName: lastName,
+      address: {
+        country: country,
+        city: city,
+      },
+      greet: function () {
+        console.log(`Hello, I am ${this.firstName} ${this.lastName}.`);
+      },
+    };
+  }
+  
+  const ola = createPerson('Ola', 'Nordmann', 'Norway', 'Oslo');
+  ola.greet();
+
+  //LESSON TASK
+  function createAnimal(name, species, food) {
+    return {
+        name: name,
+        species: species,
+        food: food,
+        animalEats: function () {
+            console.log(`${name} is a ${species} and eats ${food} food`);
+        },
+    };
+  };
+
+  const myAnimal = createAnimal("Anton", "dog", "dog food");
+  myAnimal.animalEats();
