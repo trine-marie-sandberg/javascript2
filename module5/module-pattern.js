7fcefbce59662c341bf04d68969d804df53f6fab
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-28 13:10:35 +0100

    lesson notes

---
+++ module5/module-pattern.js
@@ -0,0 +1,3 @@
+//The module pattern is similar to the object literal, and it makes use of the IIFE pattern
+//IIFE = Immediately-Invoked Function Expressions
+//annonymous function imidiately called with (); at the end
