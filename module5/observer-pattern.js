693ababc0393e50e85e532c8ee915ae743e757c3
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-11-28 17:52:11 +0100

    lesson notes

--- module5/observer-pattern.js
+++ module5/observer-pattern.js
@@ -1,2 +1,6 @@
 //The Observer pattern is an OOP (Object-Oriented Programming) pattern
 //It works by having one object be notified when another object changes
+//There are three main components to the observer pattern:
+//1.The Subject: Maintains a list of observers and manages adding/removing of observers
+//2.The Observer: An interface for interacting with the objects
+//3.The Objects: These are objects that have been linked to the observer
